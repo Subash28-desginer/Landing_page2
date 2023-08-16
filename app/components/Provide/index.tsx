010e51a88f232f25b61e96100ff73a63423cf712
Hemapriya17 <hema.learny@gmail.com>
2023-08-16 12:48:54 +0530

    Updates

--- app/components/Provide/index.tsx
+++ app/components/Provide/index.tsx
@@ -29,7 +29,7 @@ const Provide = () => {
                     <div className='flex justify-center col-span-6'>
                         <div className="flex flex-col justify-center p-10 align-middle">
                             <p className="pt-4 mt-5 text-4xl font-semibold text-center lg:text-6xl lh-81 lg:text-start">One Unified Platform</p>
-                            <h4 className="pt-4 text-lg font-normal text-center lh-33 lg:text-start text-bluegray">No more juggling multiple tools and platforms. With Twinmo's Testrunz, you have a single unified platform for every aspect of product development. This includes all stakeholders - engineers, lab managers, technicians, research, development, and production teams. Streamline every type of experiment or test, and manage all units under test, from components to subsystems to the full product.</h4>
+                            <h4 className="pt-4 text-lg font-normal text-center lh-33 lg:text-start text-bluegray">No more juggling multiple tools and platforms. With Twinmo Testrunz, you have a single unified platform for every aspect of product development. This includes all stakeholders - engineers, lab managers, technicians, research, development, and production teams. Streamline every type of experiment or test, and manage all units under test, from components to subsystems to the full product.</h4>
                             <Link href={'/'} className="flex gap-2 mx-auto mt-4 text-xl font-medium text-blue lg:mx-0 space-links">Learn more <Image src={'/assets/provide/arrow.svg'} alt={'arrow'} width={20} height={20} /></Link>
                         </div>
                     </div>
